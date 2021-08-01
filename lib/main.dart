377790553b25110c2dcca372653a901daf9fe5dc
Pawaret Muengkaew <62021393@up.ac.th>
2021-08-02 02:48:55 +0700

    Add education and experience section

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_web_browser/flutter_web_browser.dart';
 
 void main() {
   runApp(Resume());
@@ -41,16 +42,11 @@ class Body extends StatelessWidget {
               SizedBox(height: 30),
               Social(),
               SizedBox(height: 30),
-              Skills(),
+              Experiences(),
               SizedBox(height: 25),
-              Text(
-                'Experiences',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Text(
-                'Education',
-                style: Theme.of(context).textTheme.headline2,
-              ),
+              Skills(),
+              SizedBox(height: 30),
+              Education(),
             ],
           ),
         ),
@@ -59,30 +55,148 @@ class Body extends StatelessWidget {
   }
 }
 
-class Skills extends StatelessWidget {
-  final List<String> technicalSkills = [
-    'Data Structures & Algorithms',
-    'Programming Languages',
-    'Version Control',
-    'Databases'
-  ];
-  final List<String> softSkills = [
-    'Reasoning & Problem Solving',
-    'Collaboration',
-    'Communication',
-    'Active learning',
-    'Adaptability'
-  ];
-  final List<String> versionControl = ['GitHub', 'GitLab'];
-  final List<String> languages = [
-    'Javascript',
-    'Kotlin',
-    'Java',
-    'Dart',
-    'Python'
-  ];
-  final List<String> databases = ['MongoDB', 'SQL'];
+class Education extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            SvgPicture.asset(
+              'assets/icons/education.svg',
+              color: Colors.yellow[700],
+              width: 25,
+            ),
+            SizedBox(width: 10),
+            Text(
+              'Education',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          ],
+        ),
+        SizedBox(
+          child: Divider(
+            color: Colors.white,
+            thickness: 0.2,
+          ),
+        ),
+        SizedBox(height: 10),
+        InfoDetails(
+          date: '2019 - Present',
+          title: 'Bachelor of Engineering in Software Engineering',
+          subTitle: 'University of Phayao',
+          color: Colors.yellow[700],
+        ),
+        SizedBox(height: 15),
+        InfoDetails(
+          date: '2009 - 2019',
+          title: 'Sceience and Mathematics',
+          subTitle: 'Chiangrai Vidhayakhome School',
+        ),
+      ],
+    );
+  }
+}
 
+class Experiences extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            SvgPicture.asset(
+              'assets/icons/experience.svg',
+              color: Colors.yellow[700],
+              width: 25,
+            ),
+            SizedBox(width: 10),
+            Text(
+              'Experiences',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          ],
+        ),
+        SizedBox(
+          child: Divider(
+            color: Colors.white,
+            thickness: 0.2,
+          ),
+        ),
+        SizedBox(height: 10),
+        InfoDetails(
+          date: '03/2021',
+          title: 'Hackaton Boot Camp (Attend)',
+          subTitle: '20Scoops CNX Co., Ltd.,',
+        ),
+        SizedBox(height: 15),
+        InfoDetails(
+          date: '04 - 05/2021',
+          title: 'Backend Developer (Internship)',
+          subTitle: 'OneDee Solution Co., Ltd.,',
+        ),
+      ],
+    );
+  }
+}
+
+class InfoDetails extends StatelessWidget {
+  const InfoDetails({
+    Key? key,
+    required this.date,
+    required this.title,
+    required this.subTitle,
+    this.color = Colors.grey,
+  }) : super(key: key);
+
+  final String date;
+  final String title;
+  final String subTitle;
+  final color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 120,
+          height: 30,
+          decoration: BoxDecoration(
+              border: Border.all(width: 1, color: (color)!),
+              borderRadius: BorderRadius.circular(5)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(date,
+                  style: Theme.of(context)
+                      .textTheme
+                      .apply(bodyColor: color)
+                      .bodyText2),
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(
+          title,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        SizedBox(height: 5),
+        Text(
+          subTitle,
+          style: Theme.of(context)
+              .textTheme
+              .apply(bodyColor: Colors.grey[400])
+              .bodyText1,
+        ),
+      ],
+    );
+  }
+}
+
+class Skills extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -92,10 +206,10 @@ class Skills extends StatelessWidget {
           children: [
             Row(
               children: [
-                Icon(
-                  Icons.lightbulb_outline,
+                SvgPicture.asset(
+                  'assets/icons/code.svg',
                   color: Colors.yellow[700],
-                  size: 25,
+                  width: 25,
                 ),
                 SizedBox(width: 10),
                 Text(
@@ -110,11 +224,31 @@ class Skills extends StatelessWidget {
                 thickness: 0.2,
               ),
             ),
-            SkillDetails(
-                skills: technicalSkills,
-                languages: languages,
-                versionControl: versionControl,
-                databases: databases),
+            SkillDetails(text: 'Data Structures & Algorithms'),
+            SizedBox(height: 5),
+            SkillDetails(text: 'Programming Languages'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'Javascript'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'Kotlin'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'Java'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'Dart'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'Python'),
+            SizedBox(height: 5),
+            SkillDetails(text: 'Version Control'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'GitHub'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'GitLab'),
+            SizedBox(height: 5),
+            SkillDetails(text: 'Databases'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'MongoDB'),
+            SizedBox(height: 5),
+            SkillDetails2(text: 'SQL'),
           ],
         ),
         SizedBox(height: 10),
@@ -122,10 +256,10 @@ class Skills extends StatelessWidget {
           children: [
             Row(
               children: [
-                Icon(
-                  Icons.chat,
+                SvgPicture.asset(
+                  'assets/icons/chat.svg',
                   color: Colors.yellow[700],
-                  size: 25,
+                  width: 25,
                 ),
                 SizedBox(width: 10),
                 Text(
@@ -140,7 +274,15 @@ class Skills extends StatelessWidget {
                 thickness: 0.2,
               ),
             ),
-            SkillDetails(skills: softSkills),
+            SkillDetails(text: 'Reasoning & Problem Solving'),
+            SizedBox(height: 5),
+            SkillDetails(text: 'Collaboration'),
+            SizedBox(height: 5),
+            SkillDetails(text: 'Communication'),
+            SizedBox(height: 5),
+            SkillDetails(text: 'Active learning'),
+            SizedBox(height: 5),
+            SkillDetails(text: 'Adaptability'),
           ],
         ),
       ],
@@ -151,112 +293,57 @@ class Skills extends StatelessWidget {
 class SkillDetails extends StatelessWidget {
   const SkillDetails({
     Key? key,
-    required this.skills,
-    this.languages = const [],
-    this.versionControl = const [],
-    this.databases = const [],
+    required this.text,
   }) : super(key: key);
 
-  final List<String> skills, languages, versionControl, databases;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
-        for (var i in skills)
-          Column(
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.check,
-                    color: Colors.yellow[700],
-                    size: 25,
-                  ),
-                  SizedBox(width: 10),
-                  Flexible(
-                    child: Text(
-                      i,
-                      style: Theme.of(context).textTheme.bodyText2,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 5),
-              if (i == 'Programming Languages')
-                for (var i in languages)
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          SizedBox(width: 30),
-                          Icon(
-                            Icons.add,
-                            color: Colors.yellow[700],
-                            size: 20,
-                          ),
-                          SizedBox(width: 10),
-                          Flexible(
-                            child: Text(
-                              i,
-                              style: Theme.of(context).textTheme.bodyText2,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 5),
-                    ],
-                  ),
-              if (i == 'Version Control')
-                for (var i in versionControl)
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          SizedBox(width: 30),
-                          Icon(
-                            Icons.add,
-                            color: Colors.yellow[700],
-                            size: 20,
-                          ),
-                          SizedBox(width: 10),
-                          Flexible(
-                            child: Text(
-                              i,
-                              style: Theme.of(context).textTheme.bodyText2,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 5),
-                    ],
-                  ),
-              if (i == 'Databases')
-                for (var i in databases)
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          SizedBox(width: 30),
-                          Icon(
-                            Icons.add,
-                            color: Colors.yellow[700],
-                            size: 20,
-                          ),
-                          SizedBox(width: 10),
-                          Flexible(
-                            child: Text(
-                              i,
-                              style: Theme.of(context).textTheme.bodyText2,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 5),
-                    ],
-                  ),
-            ],
+        Icon(
+          Icons.check,
+          color: Colors.yellow[700],
+          size: 25,
+        ),
+        SizedBox(width: 10),
+        Flexible(
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.bodyText2,
           ),
+        ),
+      ],
+    );
+  }
+}
+
+class SkillDetails2 extends StatelessWidget {
+  const SkillDetails2({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 30),
+        Icon(
+          Icons.add,
+          color: Colors.yellow[700],
+          size: 20,
+        ),
+        SizedBox(width: 10),
+        Flexible(
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
+        ),
       ],
     );
   }
@@ -315,11 +402,11 @@ class Social extends StatelessWidget {
           width: 60,
           height: 60,
           decoration: BoxDecoration(
-              color: Colors.grey[700], borderRadius: BorderRadius.circular(10)),
+              color: Colors.grey[800], borderRadius: BorderRadius.circular(10)),
           child: Transform.scale(
             scale: 0.8,
             child: IconButton(
-                onPressed: null,
+                onPressed: openFacebook,
                 icon: SvgPicture.asset(
                   'assets/icons/facebook.svg',
                   color: Colors.yellow[700],
@@ -331,15 +418,16 @@ class Social extends StatelessWidget {
           width: 60,
           height: 60,
           decoration: BoxDecoration(
-              color: Colors.grey[700], borderRadius: BorderRadius.circular(10)),
+              color: Colors.grey[800], borderRadius: BorderRadius.circular(10)),
           child: Transform.scale(
             scale: 0.8,
             child: IconButton(
-                onPressed: null,
-                icon: SvgPicture.asset(
-                  'assets/icons/github.svg',
-                  color: Colors.yellow[700],
-                )),
+              onPressed: openGitHub,
+              icon: SvgPicture.asset(
+                'assets/icons/github.svg',
+                color: Colors.yellow[700],
+              ),
+            ),
           ),
         ),
         SizedBox(width: 20),
@@ -347,18 +435,43 @@ class Social extends StatelessWidget {
           width: 60,
           height: 60,
           decoration: BoxDecoration(
-              color: Colors.grey[700], borderRadius: BorderRadius.circular(10)),
+              color: Colors.grey[800], borderRadius: BorderRadius.circular(10)),
           child: Transform.scale(
             scale: 0.8,
             child: IconButton(
-                onPressed: null,
-                icon: SvgPicture.asset(
-                  'assets/icons/gitlab.svg',
-                  color: Colors.yellow[700],
-                )),
+              onPressed: openGitLab,
+              icon: SvgPicture.asset(
+                'assets/icons/gitlab.svg',
+                color: Colors.yellow[700],
+              ),
+            ),
           ),
         )
       ],
+    );
+  }
+
+  openFacebook() async {
+    await FlutterWebBrowser.openWebPage(
+      url: 'https://www.facebook.com/GongPawaret/',
+      customTabsOptions:
+          CustomTabsOptions(colorScheme: CustomTabsColorScheme.dark),
+    );
+  }
+
+  openGitHub() async {
+    await FlutterWebBrowser.openWebPage(
+      url: 'https://github.com/JerGun',
+      customTabsOptions:
+          CustomTabsOptions(colorScheme: CustomTabsColorScheme.dark),
+    );
+  }
+
+  openGitLab() async {
+    await FlutterWebBrowser.openWebPage(
+      url: 'https://gitlab.com/JerGun',
+      customTabsOptions:
+          CustomTabsOptions(colorScheme: CustomTabsColorScheme.dark),
     );
   }
 }
